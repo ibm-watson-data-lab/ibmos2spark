6f71858fda8cf788ad43c22ea83901e6cb671452
gadamc <adamcox@us.ibm.com>
2017-01-17 22:50:44 -0800

    Removes old swift protocol support.
    Updates and improves README.

--- r/R/osconfig.R
+++ r/R/osconfig.R
@@ -3,62 +3,9 @@ setOldClass("jobj")
 
 
 swifturl = function(name, container_name, object_name){
-  if ( grepl('_',name)){
-    stop(paste0('The swift protocol does not support underscores (_) in "name" ', paste0(name)))
-  }
-  if (grepl('_',container_name)){
-    stop(paste0('The swift protocol does not support underscores (_) in "container" ', paste0(container_name),'. Instead use swift2d via softlayer2d or bluemix2d objects.'))
-  }
-  return(paste0('swift://',container_name,'.',name,'/',object_name))
-}
-
-
-swifturl2d = function(name, container_name, object_name){
   return(paste0('swift2d://',container_name,'.',name,'/',object_name))
 }
 
-
-#'    sparkcontext is a SparkContext object.
-#' 
-#'    name is a string that identifies this configuration. You can
-#'        use any string you like. This allows you to create
-#'        multiple configurations to different Object Storage accounts.
-#'
-#'    auth_url, username and password are string credentials for your
-#'    Softlayer Object Store
-#' @export softlayer      
-#' @exportClass softlayer
-    
-softlayer <- setRefClass("softlayer",
-  fields=list(name="character", container_name="character", object_name="character",
-             sparkcontext='jobj', auth_url="character", region="character", 
-               username="character", password="character"),
-  methods=list(initialize = 
-    function( sparkcontext, name, auth_url, region, username, password,public=FALSE){     
-
-      if ( grepl('_',name)){
-        stop(paste0('The swift protocol does not support underscores (_) in "name" ', paste0(name)))
-      }
-        .self$name = name 
-        prefix = paste("fs.swift.service" , name, sep =".")
-        hConf = SparkR:::callJMethod(sparkcontext, "hadoopConfiguration")
-        SparkR:::callJMethod(hConf, "set", paste(prefix, "auth.url", sep='.'),auth_url)
-        SparkR:::callJMethod(hConf, "set", paste(prefix, "auth.endpoint.prefix", sep='.'), "endpoints")
-        SparkR:::callJMethod(hConf, "set", paste(prefix, "tenant", sep='.'), username)
-        SparkR:::callJMethod(hConf, "set", paste(prefix, "username", sep='.'), username)
-        SparkR:::callJMethod(hConf, "set", paste(prefix, "password", sep='.'), password)
-        SparkR:::callJMethod(hConf, "set", paste(prefix, "apikey", sep='.'), password)
-        SparkR:::callJMethod(hConf, "set", paste(prefix, "use.get.auth", sep='.'), "true")
-        SparkR:::callJMethod(hConf, "set", paste(prefix, "apikey", sep='.'), password)
-        SparkR:::callJMethod(hConf, "set", paste(prefix, "http.port", sep='.'), "8080")
-        SparkR:::callJMethod(hConf, "set", paste(prefix, "region", sep='.'), region)
-        invisible(SparkR:::callJMethod(hConf, "setBoolean", paste(prefix, "public", sep='.'), public))
-        invisible(SparkR:::callJMethod(hConf, "setBoolean", paste(prefix, "location-aware", sep='.'), FALSE))},
-               
-        url = function( container_name, object_name){
-        return(swifturl(name, container_name, object_name))}
-    )
-)
 
 
 #' sparkcontext is a SparkContext object.
@@ -71,7 +18,7 @@ softlayer <- setRefClass("softlayer",
 #' @export softlayer2d        
 #' @exportClass softlayer2d
 
-softlayer2d <- setRefClass("softlayer2d",
+softlayer <- setRefClass("softlayer",
   fields=list(name="character", container_name="character", object_name="character",
              sparkcontext='jobj', auth_url="character", 
               tenant = "character", username="character", password="character"),
@@ -103,66 +50,6 @@ softlayer2d <- setRefClass("softlayer2d",
     )
 )
 
-#' sparkcontext:  a SparkContext object.
-#' 
-#' credentials:  a dictionary with the following required keys:
-#'   
-#'   auth_url
-#'   project_id (or projectId)
-#'   user_id (or userId)
-#'   password
-#'   region
-#' and optional key:
-#'   name  #[to be deprecated] The name of the configuration.
-#' name:  string that identifies this configuration. You can
-#'     use any string you like. This allows you to create
-#'     multiple configurations to different Object Storage accounts.
-#'     This is not required at the moment, since credentials['name']
-#'     is still supported.
-#' When using this from a IBM Spark service instance that
-#' is configured to connect to particular Bluemix object store
-#' instances, the values for these credentials can be obtained
-#' by clicking on the 'insert to code' link just below a data
-#' source.
-#' @export bluemix      
-#' @exportClass bluemix
-
-
-bluemix <- setRefClass("bluemix",
-  fields=list(name="character", credentials = "list", 
-             sparkcontext='jobj', public = "character"),
-  methods=list(initialize = 
-    function(..., sparkcontext, name=NULL, credentials,public=FALSE){     
-
-       callSuper(...,credentials=credentials)
-
-      if ( is.null(name)) name <<- credentials["name"][[1]]
-          
-      if ( grepl('_',name)){
-        stop(paste0('The swift protocol does not support underscores (_) in "name" ', paste0(name)))
-      }
-          
-      user_id = try( credentials['user_id'][[1]])
-      if(class(user_id)=="try-error")  user_id = credentials['userId'][[1]]
-          
-      tenant = try( credentials['project_id'][[1]])
-      if(class(tenant)=="try-error")  tenant = credentials['projectId'][[1]]
-
-        .self$name = name 
-        prefix = paste("fs.swift.service" , name, sep =".")
-        hConf = SparkR:::callJMethod(sparkcontext, "hadoopConfiguration")
-        SparkR:::callJMethod(hConf, "set", paste(prefix, "auth.url", sep='.'), paste(credentials['auth_url'][[1]],"/v3/auth/tokens",sep=""))
-        SparkR:::callJMethod(hConf, "set", paste(prefix, "auth.endpoint.prefix", sep='.'), "endpoints")
-        SparkR:::callJMethod(hConf, "set", paste(prefix, "tenant", sep='.'), tenant)
-        SparkR:::callJMethod(hConf, "set", paste(prefix, "username", sep='.'), user_id)
-        SparkR:::callJMethod(hConf, "set", paste(prefix, "password", sep='.'), credentials['password'][[1]])
-        SparkR:::callJMethod(hConf, "set", paste(prefix, "region", sep='.'), credentials['region'][[1]])
-        invisible(SparkR:::callJMethod(hConf, "setBoolean", paste(prefix, "public", sep='.'), public))},
-
-        url = function( container_name, object_name){
-        return(swifturl(name, container_name, object_name))}
-    )
-)
 
           
 #' sparkcontext:  a SparkContext object.
@@ -190,7 +77,7 @@ bluemix <- setRefClass("bluemix",
 #' @exportClass bluemix2d
 
           
-bluemix2d <- setRefClass("bluemix2d",
+bluemix <- setRefClass("bluemix",
   fields=list(name="character", credentials = "list", 
              sparkcontext='jobj', public = "character"),
   methods=list(initialize = 
--- r/README.md
+++ r/README.md
@@ -1,84 +1,54 @@
 # ibmos2spark
 
-The package configures Spark Hadoop configurations for connecting to 
-Softlayer and IBM Bluemix Object Stores with the 'swift' or 'swift2d' protocol. 
-
-It is recommended to use 'swift2d' instead of the previous version. 
-The new version allows for more flexible names (underscores) and is faster.
+The package sets Spark Hadoop configurations for connecting to 
+Softlayer and IBM Bluemix Object Stores with the swift protocol. This packages
+uses the new 'swift2d' protocol. 
 
 ## Installation 
     library(devtools)
-    devtools::install_url("https://github.com/ibm-cds-labs/ibmos2spark/archive/adding_r_scala_platform.zip", subdir= "r")
+    devtools::install_url("https://github.com/ibm-cds-labs/ibmos2spark/archive/master.zip", subdir= "r")
 
 ## Usage
 
 ### Softlayer
 
-#### Using Swift (version 1) Driver
     library(ibmos2spark)
-    slsc = softlayer(sparkcontext=sc, 
-                 name="XXXXX", 
+    configurationname = "softlayerOScon" #can be any any name you like (allows for multiple configurations)
+
+    slconfig = softlayer(sparkcontext=sc, 
+                 name=configurationname, 
                  auth_url="https://identity.open.softlayer.com",
-                 region="XXXXX", 
                  tenant = "XXXXX", 
                  username="XXXXX", 
                  password="XXXXX"
            )
            
-    data <- read.df(sqlContext, slsc$url(name, space,object), source = "com.databricks.spark.csv", header = "true")
-    head(data)
+    container = "my_container"
+    object = "my_data.csv"
 
-
-#### Using Swift2d Driver
-
-    library(ibmos2spark)
-    slsc = softlayer2d(sparkcontext=sc, 
-                 name="XXXXX", 
-                 auth_url="https://identity.open.softlayer.com",
-                 region="XXXXX", 
-                 tenant = "XXXXX", 
-                 username="XXXXX", 
-                 password="XXXXX"
-           )
-           
-    data <- read.df(sqlContext, slsc$url(name, space,object), source = "com.databricks.spark.csv", header = "true")
+    data <- read.df(sqlContext, slconfig$url(container,object), source = "com.databricks.spark.csv", header = "true")
     head(data)
 
 ### Bluemix
 
-#### Using Swift Driver
-
     library(ibmos2spark)
+    configurationname = "bluemixOScon" #can be any any name you like (allows for multiple configurations)
 
-    creds = list(name="XXXXX", 
-             auth_url="https://identity.open.softlayer.com",
+    # In DSX notebooks, the "insert to code" will insert this credentials list for you
+    creds = list(
+            auth_url="https://identity.open.softlayer.com",
             region="dallas", 
             project_id = "XXXXX", 
             user_id="XXXXX", 
             password="XXXXX")
             
+    bmconfig = bluemix(sparkcontext=sc, name=configurationname, credentials = creds)
 
-    bmsc = bluemix(sparkcontext=sc, name=name, credentials = creds)
+    container = "my_container"
+    object = "my_data.csv"
 
-    data <- read.df(sqlContext, bmsc$url(name, space,object), source = "com.databricks.spark.csv", header = "true")
-
+    data <- read.df(sqlContext, bmconfig$url(container,object), source = "com.databricks.spark.csv", header = "true")
     head(data)
-
-
-#### Using Swift2d Driver
-
-    library(ibmos2spark)
-    creds = list(name="XXXXX", 
-             auth_url="https://identity.open.softlayer.com",
-            region="dallas", 
-            project_id = "XXXXX", 
-            user_id="XXXXX", 
-            password="XXXXX")
-            
-    bmsc = bluemix2d(sparkcontext=sc, name=name, credentials = creds)
-
-    data <- read.df(sqlContext, bmsc$url(name, space,object), source = "com.databricks.spark.csv", header = "true")
-head(data)
 
 
 ## License 
